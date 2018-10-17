0eea577a994bef0bf926d2c9956fd61b8a8ef8bb
Seth Dana <seth.dana@gmail.com>
2018-10-17 10:37:25 -0500

    Tried testing...failed miserably

--- BangazonTests/UnitTest1.cs
+++ BangazonTests/UnitTest1.cs
@@ -1,13 +1,43 @@
 using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
 using Xunit;
 
 namespace BangazonTests
 {
-    public class UnitTest1
+    public class UnitTest1 : IClassFixture<WebApplicationFactory<Bangazon.Startup>>
     {
+        private readonly WebApplicationFactory<Bangazon.Startup> _factory;
+
+        public UnitTest1(WebApplicationFactory<Bangazon.Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        //[InlineData("/")]
+        [InlineData("/Products")]
+        //[InlineData("/Products/Types")]
+        //[InlineData("/Products/Create")]
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("text/html; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+
         [Fact]
         public void Test1()
         {
+
 
         }
     }
